092c2989d46c567854c41aae13e77f9980c8499d
Emagmunioz <emagmunioz@gmail.com>
2025-03-11 12:36:04 +0100

    Create HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar/navbar';
-import Body from './components/body/Body.jsx';
-import Footer from './components/footer/Footer';
 import './App.css';
-
+import HomePage from './pages/HomePage';
 function App() {
     return(
       <div className="app">
-        <Navbar />  
-        <Body />
-        <Footer />
+        <HomePage/>
       </div>
     )
 }
