050deaa4f52c2097370b09092f5d54515140dcac
Ayuik <marcosayelen@gmail.com>
2025-03-13 08:35:34 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import ResourcesPage from './pages/ResourcesPage';  
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 
 function App() {
-
-        return(
+    
+    return(
         <div className="app">
-            <ResourcesPage />
-        </div>
+<Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/resources" element={<ResourcesPage />} />
+        </Routes>
+    </Router>        
+    </div>
     );
 }
-
-export default App;
